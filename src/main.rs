6c779662042931e87f44bd85cb23764e3e0b710a
Griffin Sectio <grifin.dwi13@gmail.com>
2024-06-16 16:38:37 +0700

    add /music

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod commands;
 
 use std::env;
+use std::process;
+use std::fs;
 
 use serde_json::{self, Value};
 
@@ -14,6 +16,8 @@ use serenity::all::CreateMessage;
 use serenity::builder::CreateAttachment;
 
 use poise::{serenity_prelude as serenity, ChoiceParameter, CreateReply};
+
+use regex::Regex;
 
 
 struct Data {} // User data, which is stored and accessible in all command invocations
@@ -296,6 +300,49 @@ async fn cat(
     Ok(())
 }
 
+#[poise::command(slash_command, description_localized("en-US", "Download music from YouTube, YouTube Music, etc."))]
+async fn music(
+    ctx: Context<'_>,
+    #[description = "The url of the music"]
+    url: String
+) -> Result<(), Error> {
+    let fetching = ctx.say("Downloading the music for you...").await.unwrap();
+    
+    let _yt_dlp = process::Command::new("./src/yt-dlp")
+    .arg("-S acodec:m4a")
+    .arg("-f ba")
+    .arg("-o %(title)s.%(ext)s")
+    .arg("-P ./")
+    .arg(url)
+    .output()
+    .unwrap();
+
+    let re = Regex::new(r".*\.m4a").unwrap();
+
+    let directory = fs::read_dir("./").unwrap();
+
+    println!("{}", std::str::from_utf8(&_yt_dlp.stdout).unwrap());
+    println!("{}", std::str::from_utf8(&_yt_dlp.stderr).unwrap());
+
+    for dir_entry in directory {
+        let entry_path = dir_entry.unwrap().path();
+        let music_path = entry_path.to_str().unwrap();
+
+        if re.is_match(music_path) {
+            let music_attachment = CreateAttachment::path(music_path).await.unwrap();
+            let music = CreateMessage::new()
+            .add_file(music_attachment);
+
+            ctx.channel_id().send_message(&ctx.http(), music).await.unwrap();
+            std::fs::remove_file(music_path).unwrap();
+        }
+    }
+    
+    fetching.delete(ctx).await.unwrap();
+
+    Ok(())
+}
+
 #[poise::command(prefix_command)]
 pub async fn register(
     ctx: Context<'_>
@@ -314,7 +361,7 @@ async fn main() {
 
     let framework = poise::Framework::builder()
     .options(poise::FrameworkOptions {
-        commands: vec![register(), ping(), advice(), sticker(), gif(), image(), quote(), joke(), cat()],
+        commands: vec![register(), ping(), advice(), sticker(), gif(), image(), quote(), joke(), cat(), music()],
         ..Default::default()
     })
     .setup(|ctx, _ready, framework| {
